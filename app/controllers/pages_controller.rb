35ced331bcb3020888b5e8d3b1c136e6f4824576
Adam Gibson <adamericgibson@gmail.com>
2015-11-16 21:37:35 +0000

    Commenting controller and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,14 @@ class PagesController < ApplicationController
     if params[:x].present?
     @input = "#{params[:x]}"
     @input = @input.upcase
-    @input2 = @input.split(" ").map {|a| "'#{a}',\n"}.join # works OK
-    @input3 = @input2[0,(@input2.length-2)]
+    # Convert user list to upper case
+    @input2 = @input.split(" ").map {|a| "'#{a}',\n"}.join
+    # splits user input by white space,
+    # wraps each item in single quotes and puts next item on a new line
+    @userOutput = @input2[0,(@input2.length-2)]
+    # takes substring of user input, and removes final comma as
+    # SQL will not parse correctly if final list item has a comma after it.
+
     end
 
   end
